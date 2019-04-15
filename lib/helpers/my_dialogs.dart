f253a50e91c22693455d3600c016618cf79ad0fd
Benjamin <b3njamin.bozic@gmail.com>
2019-04-16 00:22:02 +0100

    color scheme fix added popup before sending data back button fix

--- lib/helpers/my_dialogs.dart
+++ lib/helpers/my_dialogs.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:access_settings_menu/access_settings_menu.dart';
+import 'package:recappture2/helpers/my_colors.dart';
+import 'package:recappture2/model/my_data.dart';
+import 'dart:io';
 
 Widget loadingDialog = WillPopScope(
   onWillPop: () async {
@@ -9,14 +12,14 @@ Widget loadingDialog = WillPopScope(
     content: Row(
       children: <Widget>[
         CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(Colors.lightGreenAccent),
+          valueColor: AlwaysStoppedAnimation<Color>(MyColors.green),
         ),
         Padding(
           padding: EdgeInsets.only(left: 20.0),
           child: Text(
-            "Prosimo počakajte",
+            "Prosimo počakajte ...",
             style: TextStyle(
-              color: Colors.grey,
+              color: MyColors.grey,
               fontSize: 20.0,
             ),
           ),
@@ -34,14 +37,14 @@ Widget locationDialog = WillPopScope(
     content: Row(
       children: <Widget>[
         CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(Colors.lightGreenAccent),
+          valueColor: AlwaysStoppedAnimation<Color>(MyColors.green),
         ),
         Padding(
           padding: EdgeInsets.only(left: 20.0),
           child: Text(
             "Iskanje lokacije ...",
             style: TextStyle(
-              color: Colors.grey,
+              color: MyColors.grey,
               fontSize: 20.0,
             ),
           ),
@@ -62,8 +65,7 @@ Widget exitDialog(BuildContext context) {
         child: ListBody(
           children: <Widget>[
             Text(
-              'Želite zapreti aplikacijo?',
-              style: TextStyle(color: Colors.grey),
+              'Želite zapreti aplikacijo?', style: TextStyle(color: MyColors.green, fontWeight: FontWeight.bold),
             ),
           ],
         ),
@@ -72,7 +74,7 @@ Widget exitDialog(BuildContext context) {
         FlatButton(
           child: Text(
             'PREKLIČI',
-            style: TextStyle(color: Colors.lightGreenAccent),
+            style: TextStyle(color: MyColors.green),
           ),
           onPressed: () {
             Navigator.pop(context);
@@ -81,9 +83,10 @@ Widget exitDialog(BuildContext context) {
         FlatButton(
           child: Text(
             'POTRDI',
-            style: TextStyle(color: Colors.lightGreenAccent),
+            style: TextStyle(color: MyColors.green),
           ),
           onPressed: () {
+            exit(0);
           },
         ),
       ],
@@ -108,13 +111,13 @@ Widget openSettings(BuildContext context) {
       return new Future(() => false);
     },
     child: AlertDialog(
-      title: Text('Lokacija onemogočena'),
+      title: Text('Lokacija onemogočena', style: TextStyle(color: MyColors.green, fontWeight: FontWeight.bold),),
       content: SingleChildScrollView(
         child: ListBody(
           children: <Widget>[
             Text(
               'Želite odpreti nastavitve lokacije?',
-              style: TextStyle(color: Colors.grey),
+              style: TextStyle(color: MyColors.grey),
             ),
           ],
         ),
@@ -123,7 +126,7 @@ Widget openSettings(BuildContext context) {
         FlatButton(
           child: Text(
             'PREKLIČI',
-            style: TextStyle(color: Colors.lightGreenAccent),
+            style: TextStyle(color: MyColors.green),
           ),
           onPressed: () {
             Navigator.pop(context);
@@ -132,7 +135,7 @@ Widget openSettings(BuildContext context) {
         FlatButton(
           child: Text(
             'ODPRI',
-            style: TextStyle(color: Colors.lightGreenAccent),
+            style: TextStyle(color: MyColors.green),
           ),
           onPressed: () {
             openSettingsMenu();
