0e30cb8ef8702911fb8d70614396c5fe82e2cc8e
Benjamin <b3njamin.bozic@gmail.com>
2019-04-15 17:00:16 +0100

    color scheme fix

--- lib/helpers/my_colors.dart
+++ lib/helpers/my_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static Color green = Color(0xffBBD32E);
-  static Color grey = Color(0xff9d9fa2);
+  static Color green = Color(0xffadf700);
+  static Color grey = Color(0xffb3b3b3);
   static Color accent = Colors.lightGreen;
   static Color primary = Colors.grey;
   static Brightness brightness = Brightness.light;
